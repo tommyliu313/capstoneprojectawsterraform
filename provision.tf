411488213c1424929a803e777534b88dd6597c63
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-20 16:15:47 +0800

    20220420 1615 update

---
+++ provision.tf
@@ -0,0 +1,12 @@
+provisioner "remote-exec"{
+  inline = [
+  "git clone https://github.com/baselm/capstoneproject.git",
+  "   unzip Example.zip -d /var/www/html/",
+  "sudo yum -y update",
+  "sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2",
+  "sudo yum install -y httpd mariadb-server",
+  "sudo systemctl start httpd",
+  "sudo systemctl enable httpd",
+  "sudo systemctl is-enabled httpd"
+  ]
+}
--- resource.tf
+++ resource.tf
@@ -1,6 +1,20 @@
 # security group
 resource "aws_security_group" "Example-DBSG"{
-description = "Enable access to MySQL"
+  name = "example database security group"
+  vpc_id = module.vpc.vpc_id
+  description = "Enable access to MySQL"
+  ingress {
+
+    from_port = 0
+    protocol  = "MYSQL/Aurora"
+    to_port   = 3306
+  }
+  egress{
+
+    from_port = 0
+    protocol  = "MySQL/Aurora"
+    to_port   =
+  }
 }
 
 resource "aws_security_group" "Bastion-SG"{
@@ -9,15 +23,6 @@ resource "aws_security_group" "Bastion-SG"{
 
 resource "aws_security_group" "Inventory-App"{
 description = "Enable access to App"
-}
-
-resource "aws_security_group_rule" "MySQL_Access_Inbound"{
-  type = "ingress"
-  protocol = "MYSQL/Aurora"
-  security_group_id = aws_security_group.Example-DBSG
-  from_port = 0
-  to_port = 3306
-  cidr_blocks = [aws_vpc.example.cidr_block]
 }
 
 # cloud9 environment
